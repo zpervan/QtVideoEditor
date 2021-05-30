e2ef284149fc509191f494de7a1d206b897a36ef
Zvonimir Pervan <z.pervan65@gmail.com>
2021-05-30 15:20:18 +0200

    minor: Refactor VideoListModel class

--- GUI/Code/video_list_model.cpp
+++ GUI/Code/video_list_model.cpp
@@ -16,10 +16,8 @@ void VideoListModel::PopulateVideoElementsList() {
     video_element_information.video_file_name = video_file.path().stem().c_str();
     video_element_information.thumbnail_file_path = thumbnail_creator::CreateThumbnailFromVideo(video_file.path());
 
-    qDebug() << "Video file name: "
-             << video_element_information.video_file_name;
-    qDebug() << "Thumbnail file name: "
-             << video_element_information.thumbnail_file_path;
+    qDebug() << "Video file name: " << video_element_information.video_file_name;
+    qDebug() << "Thumbnail file name: " << video_element_information.thumbnail_file_path;
 
     video_element_information_.append(std::move(video_element_information));
   }
@@ -37,29 +35,26 @@ QVariant VideoListModel::data(const QModelIndex &index, int role) const {
     return QVariant();
   }
 
-  auto video_element = video_element_information_.at(index.row());
-  auto video_element_role = static_cast<VideoElementRoles>(role);
+  const auto video_element = video_element_information_.at(index.row());
 
-  if (video_element_role == VideoElementRoles::kVideoFileName) {
-    return video_element.video_file_name;
+  switch (role) {
+  case VideoElementRoles::kVideoFileName:
+    return {video_element.video_file_name};
+
+  case VideoElementRoles::kThumbnailFilePath:
+    return {video_element.thumbnail_file_path};
+
+  default:
+    return QVariant();
   }
-
-  if (video_element_role == VideoElementRoles::kThumbnailFilePath) {
-    return video_element.thumbnail_file_path;
-  }
-
-  return QVariant();
 }
 
 int VideoListModel::rowCount(const QModelIndex &parent) const {
-  if (parent.isValid() || video_element_information_.empty()) {
-    return EXIT_FAILURE;
-  }
-
-  return video_element_information_.size();
+  return (parent.isValid() || video_element_information_.empty())
+             ? EXIT_FAILURE
+             : video_element_information_.size();
 }
 
-QVector<VideoElementInformation>
-VideoListModel::GetVideoElementInformation() const {
+QVector<VideoElementInformation> VideoListModel::GetVideoElementInformation() const {
   return video_element_information_;
 }
--- GUI/Code/video_list_model.h
+++ GUI/Code/video_list_model.h
@@ -4,35 +4,42 @@
 #include <QAbstractListModel>
 #include <QUrl>
 
+/// @brief Data container for storing information of a single video file
 struct VideoElementInformation {
   QString video_file_name;
   QString thumbnail_file_path;
 };
 
+/// @brief Contains all necessary logic to fetch the populated video list.
 class VideoListModel : public QAbstractListModel {
   Q_OBJECT
-  Q_PROPERTY(QVector<VideoElementInformation> VideoListModel READ
-                 GetVideoElementInformation CONSTANT)
+  Q_PROPERTY(QVector<VideoElementInformation> VideoListModel READ GetVideoElementInformation CONSTANT)
+
 public:
+  explicit VideoListModel(QObject *parent = nullptr);
+
+  /// @brief Data roles for video element information
   enum VideoElementRoles {
     kVideoFileName = Qt::UserRole + 1,
     kThumbnailFilePath
   };
 
-  explicit VideoListModel(QObject *parent = nullptr);
+public slots:
+  /// @brief Returns the specific role data from the video list element.
+  /// @param [in] index Position of the video element in the populated list
+  /// @param [in] role Data role
+  /// @return Information of a data role from a specific video list element
+  QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
 
-  int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-
-  QHash<int, QByteArray> roleNames() const override;
+  /// @brief Gets the list of video element information
+  /// @return List of video element information
+  QVector<VideoElementInformation> GetVideoElementInformation() const;
 
 private:
   void PopulateVideoElementsList();
   QVector<VideoElementInformation> video_element_information_;
-
-public slots:
-  QVariant data(const QModelIndex &index,
-                int role = Qt::DisplayRole) const override;
-  QVector<VideoElementInformation> GetVideoElementInformation() const;
+  int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+  QHash<int, QByteArray> roleNames() const override;
 };
 
 #endif // QTVIDEOEDITOR_GUI_CODE_VIDEO_LIST_MODEL_H_
